c308b563033bee2e22b895462d788564077e9beb
Max H <10329648+8ear@users.noreply.github.com>
2018-12-11 12:03:43 +0100

    Add Alpine Version 1.5 to exclude an deadlock (#13)
    ## Changelog for bug.MDD143 - Fix for function "create_certificate" deadlock Bug
    ### Update Informations 
    Proxy version 1.4-alpine creates a file to prevent the misp-server from creating its own certificate. This should be deleted once the creation is complete. Unfortunately the proxy with verison 1.4-alpine interrupted the script before removing the file. Therefore it has not been deleted anymore. Since only one file was used for both the proxy and the misp-server this bug has the consequence that the apache2 entrypoint of the misp-server version 2.4.97-2.4.99 is also deadlocked.
    
    ### General Changes
    No general changes were made.
    
    ### Fixes & Improvements
    - Added a new misp-proxy version 1.5 with a own pid file for misp-server and misp-proxy.
    
    ### Detailed Changes
    - We have added an additional pid file. So we now have one that creates the misp-proxy and queries the misp-server and one that is created by the misp-server and queries the misp-proxy. Its own file can overwrite both the proxy and the server. Therefore such deadlocks should be a thing of the past in the future.

---
+++ 1.5-alpine/files/entrypoint_nginx.sh
@@ -0,0 +1,197 @@
+#!/bin/sh
+set -e
+
+SSL_DH_FILE="/etc/nginx/ssl/dhparams.pem"
+SSL_KEY="/etc/nginx/ssl/key.pem"
+SSL_CERT="/etc/nginx/ssl/cert.pem"
+VARS_COMMON="/etc/nginx/conf.d/vars_common"
+GLOBAL_allow_IPs=/etc/nginx/conf.d/GLOBAL_allow_IPs
+HTTPS_CONFIG="/etc/nginx/conf.d/SERVER_HTTPS_and_redirected_HTTP"
+HTTP_CONFIG="/etc/nginx/conf.d/SERVER_HTTP_only"
+MAINTENANCE="/etc/nginx/conf.d/SERVER_MAINTENANCE"
+PID_CERT_CREATER="/etc/nginx/ssl/SSL_create.pid"
+MAINTENANCE_HTML_PATH="/var/www/maintenance"
+MAINTENANCE_HTML_FILE="$MAINTENANCE_HTML_PATH/index.html"
+FOLDER_with_VERSIONS="/etc/nginx/ssl /etc/nginx/conf.d"
+
+
+function SSL_generate_cert(){
+    # If a valid SSL certificate is not already created for the server, create a self-signed certificate:
+    i=0
+    while [ -f $PID_CERT_CREATER.server ]
+    do
+        echo "`date +%T` -  misp-server container create currently the certificate. misp-proxy until misp-server is finish."
+        # added to escape a deadlock from proxy 1.4-alpine with misp server 2.4.97-2.4.99.
+        i=$((i+1))
+        sleep 2
+        [ "$i" -eq 30 ] && rm $PID_CERT_CREATER.server
+        # END added to escape a deadlock from proxy 1.4-alpine with misp server 2.4.97-2.4.99.
+    done
+    
+    [ ! -f $SSL_CERT -a ! -f $SSL_KEY ] && touch $PID_CERT_CREATER.proxy && echo "Create SSL Certificate..." && openssl req -x509 -newkey rsa:4096 -keyout $SSL_KEY -out $SSL_CERT -days 365 -sha256 -subj '/CN='${HOSTNAME} -nodes && echo "finished." && rm $PID_CERT_CREATER.proxy
+    
+    echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+}
+
+function SSL_generate_DH(){
+    [ ! -f $SSL_DH_FILE ] && echo "Create DH params - This can take a long time, so take a break and enjoy a cup of tea or coffee." && openssl dhparam -out $SSL_DH_FILE 2048
+    echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+}
+
+function deactivate_http_config(){
+    [ -f $HTTP_CONFIG.conf ] && echo "mv $HTTP_CONFIG.conf $HTTP_CONFIG" && mv $HTTP_CONFIG.conf $HTTP_CONFIG
+    echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+}
+
+function activate_https_config()
+{
+    [ -f $HTTPS_CONFIG ] && echo "mv $HTTPS_CONFIG $HTTPS_CONFIG.conf" && mv $HTTPS_CONFIG $HTTPS_CONFIG.conf
+    echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+}
+
+function file_GLOBAL_allow_IPs(){
+IP="$1"
+
+
+if [ -z "$IP" ]
+then
+    # If no param is given allow all IP
+cat << EOF > $GLOBAL_allow_IPs
+allow all;
+EOF
+else
+    # If param is given include only the valid ips
+[ -f $GLOBAL_allow_IPs ] && rm $GLOBAL_allow_IPs
+
+for i in $IP
+do
+cat << EOF >> $GLOBAL_allow_IPs
+allow $i;
+EOF
+done
+
+fi
+
+
+chmod 644 $GLOBAL_allow_IPs
+echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+}
+
+function file_vars_common()
+{  
+cat << EOF > $VARS_COMMON
+server_name $HOSTNAME;
+client_max_body_size 50M;
+
+EOF
+chmod 644 $VARS_COMMON
+echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+}
+
+function file_maintenance_html(){
+
+[ ! -d $MAINTENANCE_HTML_PATH ] && echo "mkdir -p $MAINTENANCE_HTML_PATH" && mkdir -p $MAINTENANCE_HTML_PATH; # Add directory for maintenance File + Copy Maintenance config
+
+cat << EOF > $MAINTENANCE_HTML_FILE
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:${HTTP_SERVERADMIN}?Subject=MISP-dockerized Maintenance at ${HOSTNAME}">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+        <p>&mdash; Your MISP Support Team</p>
+    </div>
+</article>
+
+EOF
+echo
+}
+
+function enable_maintenance(){
+    # deactivate https
+    [ -f $HTTPS_CONFIG.conf ] && echo "mv $HTTPS_CONFIG.conf $HTTPS_CONFIG" && mv $HTTPS_CONFIG.conf $HTTPS_CONFIG
+    [ -f $MAINTENANCE ] && echo "mv $MAINTENANCE $MAINTENANCE.conf" && mv $MAINTENANCE $MAINTENANCE.conf
+    nginx -t
+    echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+    exit
+}
+
+function disable_maintenance(){
+    [ -f $HTTPS_CONFIG ] && mv $HTTPS_CONFIG $HTTPS_CONFIG.conf
+    [ -f $MAINTENANCE.conf ] && mv $MAINTENANCE.conf $MAINTENANCE
+    nginx -t
+    echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+    exit
+}
+
+function upgrade(){
+    for i in $FOLDER_with_VERSIONS
+    do
+        if [ ! -f $i/${NAME} ] 
+        then
+            # File not exist and now it will be created
+            echo ${VERSION} > $i/${NAME}
+        elif [ ! -f $i/${NAME} -a -z "$(cat $i/${NAME})" ]
+        then
+            # File exists, but is empty
+            echo ${VERSION} > $i/${NAME}
+        elif [ "$VERSION" == "$(cat $i/${NAME})" ]
+        then
+            # File exists and the volume is the current version
+            echo "Folder $i is on the newest version."
+        else
+            # upgrade
+            echo "Folder $i should be updated."
+            case $(echo $i/$NAME) in
+            1.4)
+                # Tasks todo in 2.4.92
+                echo "#### Upgrade Volumes from 2.4.92 ####"
+                ;;
+            *)
+                echo "Unknown Version, upgrade not possible."
+                exit
+                ;;
+            esac
+            ############ DO ANY!!!
+        fi
+    done
+    echo
+}
+
+#####################   MAIN    ###################
+# generate vars_common
+file_vars_common
+# generate global_allow_IPs
+file_GLOBAL_allow_IPs "$IP"
+# check if ssl cert is required to generate
+SSL_generate_cert
+# check if DH file is required to generate
+SSL_generate_DH
+# create maintenance file
+file_maintenance_html
+# check volumes and upgrade if it is required
+echo "upgrade if it is required..." && upgrade
+
+# activate maintenance
+[ "$1" == "enable-maintenance" ] && enable_maintenance
+
+# deactivate maintenance
+[ "$1" == "disable-maintenance" ] && disable_maintenance
+
+# test nginx config
+nginx -t
+
+# if no param is given
+[ -z "$1" ] && exec nginx -g "daemon off;"
+
+# execute any COMMAND
+exec $@
