e8ec19410ba8567bbf06b2536953b5579dd85942
8ear <github@8ear.de>
2019-04-29 12:28:15 +0200

    Add CI files

---
+++ .ci/05_notify_hub.docker.com.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+STARTMSG="[notify_hob.docker.com]"
+
+echo "$STARTMSG Notify hub.docker.com"
+
+# Docker Repo e.g. dcso/misp-dockerized-proxy
+[ -z "$(git remote get-url origin|grep git@)" ] || GIT_REPO="$(git remote get-url origin|sed 's,.*:,,'|sed 's,....$,,')"
+[ -z "$(git remote get-url origin|grep http)" ] || GIT_REPO="$(git remote get-url origin|sed 's,.*github.com/,,'|sed 's,....$,,')"
+[ -z "$(echo $GIT_REPO|grep $GITLAB_HOST)" ] ||  GIT_REPO="$(git remote get-url origin|sed 's,.*'${GITLAB_HOST}'/'${GITLAB_GROUP}'/,,'|sed 's,....$,,')"
+
+DOCKER_REPO="dcso/$(echo $GIT_REPO|cut -d / -f 2|tr '[:upper:]' '[:lower:]')"
+
+curl -X POST -H "Content-Type: application/json"  --data '{"docker_tag_name": "hub_automatic_untested"}'  https://registry.hub.docker.com/u/$DOCKER_REPO/trigger/$1/
+
+echo "$STARTMSG $0 is finished."
