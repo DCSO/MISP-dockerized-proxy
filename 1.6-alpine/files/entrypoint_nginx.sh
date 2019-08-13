326b38b4b890cec3c85668276eb2f7f31b96f381
8ear <github@8ear.de>
2019-08-13 12:01:13 +0200

    Improve 1.6 Output

--- 1.6-alpine/files/entrypoint_nginx.sh
+++ 1.6-alpine/files/entrypoint_nginx.sh
@@ -20,6 +20,10 @@ FOLDER_with_VERSIONS="/etc/nginx/ssl /etc/nginx/conf.d"
 MISP_FQDN=${MISP_FQDN:-"$HOSTNAME"}
 CLIENT_MAX_BODY_SIZE=${PROXY_CLIENT_MAX_BODY_SIZE:-"50M"}
 
+echo() {
+    command echo "$STARTMSG $*"
+}
+
 SSL_generate_cert(){
     # If a valid SSL certificate is not already created for the server, create a self-signed certificate:
     i=0
@@ -29,14 +33,14 @@ SSL_generate_cert(){
         # added to escape a deadlock from proxy 1.4-alpine with misp server 2.4.97-2.4.99.
         i=$((i+1))
         sleep 2
-        [ "$i" -eq 30 ] && rm "$PID_CERT_CREATER.server"
+        [ "$i" -eq 30 ] && rm -v "$PID_CERT_CREATER.server"
         # END added to escape a deadlock from proxy 1.4-alpine with misp server 2.4.97-2.4.99.
     done
     
     if [ ! -f "$SSL_CERT" ] && [ ! -f "$SSL_KEY" ]; then
-        touch "$PID_CERT_CREATER.proxy" && echo "$STARTMSG Create SSL Certificate..." 
+        touch "$PID_CERT_CREATER.proxy" && echo "Create SSL Certificate..." 
         openssl req -x509 -newkey rsa:4096 -keyout $SSL_KEY -out $SSL_CERT -days 365 -sha256 -subj "/C=DE/CN=${MISP_FQDN}" -nodes 
-        rm $PID_CERT_CREATER.proxy
+        rm -v $PID_CERT_CREATER.proxy
     fi
     echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
 }
@@ -46,27 +50,27 @@ SSL_generate_DH(){
     i=0
     while [ -f "$PID_CERT_CREATER.server" ]
     do
-        echo "$STARTMSG $(date +%T) -  misp-server container create currently the certificate. misp-proxy until misp-server is finish."
+        echo "$(date +%T) -  misp-server container create currently the certificate. misp-proxy until misp-server is finish."
         # added to escape a deadlock from proxy 1.4-alpine with misp server 2.4.97-2.4.99.
         i=$((i+1))
         sleep 2
-        [ "$i" -eq 30 ] && rm $PID_CERT_CREATER.server
+        [ "$i" -eq 30 ] && rm -v $PID_CERT_CREATER.server
         # END added to escape a deadlock from proxy 1.4-alpine with misp server 2.4.97-2.4.99.
     done
     
-    [ ! -f "$SSL_DH_FILE" ] && touch $PID_CERT_CREATER.proxy && echo "$STARTMSG Create DH params - This can take a long time, so take a break and enjoy a cup of tea or coffee." && openssl dhparam -out $SSL_DH_FILE 2048 && rm $PID_CERT_CREATER.proxy
-    echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+    [ ! -f "$SSL_DH_FILE" ] && touch $PID_CERT_CREATER.proxy && echo "Create DH params - This can take a long time, so take a break and enjoy a cup of tea or coffee." && openssl dhparam -out $SSL_DH_FILE 2048 && rm $PID_CERT_CREATER.proxy
+    command echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
 }
 
 deactivate_http_config(){
-    [ -f "$HTTP_CONFIG.conf" ] && echo "mv $HTTP_CONFIG.conf $HTTP_CONFIG" && mv $HTTP_CONFIG.conf $HTTP_CONFIG
-    echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+    [ -f "$HTTP_CONFIG.conf" ] && echo "mv $HTTP_CONFIG.conf $HTTP_CONFIG" && mv -v $HTTP_CONFIG.conf $HTTP_CONFIG
+    command echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
 }
 
 activate_https_config()
 {
-    [ -f "$HTTPS_CONFIG" ] && echo "mv $HTTPS_CONFIG $HTTPS_CONFIG.conf" && mv $HTTPS_CONFIG $HTTPS_CONFIG.conf
-    echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+    [ -f "$HTTPS_CONFIG" ] && echo "mv $HTTPS_CONFIG $HTTPS_CONFIG.conf" && mv -v $HTTPS_CONFIG $HTTPS_CONFIG.conf
+    command echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
 }
 
 file_GLOBAL_allow_IPs(){
@@ -81,7 +85,7 @@ allow all;
 EOF
 else
     # If param is given include only the valid ips
-[ -f "$GLOBAL_allow_IPs" ] && rm "$GLOBAL_allow_IPs"
+[ -f "$GLOBAL_allow_IPs" ] && rm -v "$GLOBAL_allow_IPs"
 
 for i in $IP
 do
@@ -94,7 +98,7 @@ fi
 
 
 chmod 644 "$GLOBAL_allow_IPs"
-echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+command echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
 }
 
 file_vars_common()
@@ -105,7 +109,7 @@ client_max_body_size $CLIENT_MAX_BODY_SIZE;
 
 EOF
 chmod 644 "$VARS_COMMON"
-echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+command echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
 }
 
 file_maintenance_html(){
@@ -133,71 +137,71 @@ cat << EOF > $MAINTENANCE_HTML_FILE
 </article>
 
 EOF
-echo
+command echo
 }
 
 enable_maintenance(){
     # deactivate https
-    [ -f "$HTTPS_CONFIG.conf" ] && echo "$STARTMSG mv $HTTPS_CONFIG.conf $HTTPS_CONFIG" && mv $HTTPS_CONFIG.conf $HTTPS_CONFIG
-    [ -f "$MAINTENANCE" ] && echo "$STARTMSG mv $MAINTENANCE $MAINTENANCE.conf" && mv $MAINTENANCE $MAINTENANCE.conf
+    [ -f "$HTTPS_CONFIG.conf" ] && echo "mv $HTTPS_CONFIG.conf $HTTPS_CONFIG" && mv -v $HTTPS_CONFIG.conf $HTTPS_CONFIG
+    [ -f "$MAINTENANCE" ] && echo "mv $MAINTENANCE $MAINTENANCE.conf" && mv -v $MAINTENANCE $MAINTENANCE.conf
     nginx -t
-    echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+    command echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
     exit
 }
 
 disable_maintenance(){
-    [ -f "$HTTPS_CONFIG" ] && echo "$STARTMSG mv $HTTPS_CONFIG $HTTPS_CONFIG.conf" && mv $HTTPS_CONFIG $HTTPS_CONFIG.conf
-    [ -f "$MAINTENANCE.conf" ] && echo "$STARTMSG mv $MAINTENANCE.conf $MAINTENANCE" && mv $MAINTENANCE.conf $MAINTENANCE
+    [ -f "$HTTPS_CONFIG" ] && echo "mv $HTTPS_CONFIG $HTTPS_CONFIG.conf" && mv -v $HTTPS_CONFIG $HTTPS_CONFIG.conf
+    [ -f "$MAINTENANCE.conf" ] && echo "mv $MAINTENANCE.conf $MAINTENANCE" && mv -v $MAINTENANCE.conf $MAINTENANCE
     nginx -t
-    echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
+    command echo # add an echo command because if no command is done busybox (alpine sh) won't continue the script
     exit
 }
 
-# upgrade(){
-#     for i in $FOLDER_with_VERSIONS
-#     do
-#         if [ ! -f "$i/${NAME}" ] 
-#         then
-#             # File not exist and now it will be created
-#             echo "${VERSION}" > "$i/${NAME}"
-#         elif [ ! -f "$i/${NAME}" ] && [ -z "$(cat "$i/${NAME}")" ]
-#         then
-#             # File exists, but is empty
-#             echo "${VERSION}" > "$i/${NAME}"
-#         elif [ "$VERSION" = "$(cat "$i/${NAME}")" ]
-#         then
-#             # File exists and the volume is the current version
-#             echo "Folder $i is on the newest version."
-#         else
-#             # upgrade
-#             echo "$STARTMSG Folder $i should be updated."
-#             case "$i/$NAME" in
-#             "1.4")
-#                 # Tasks todo in 2.4.92
-#                 echo "#### Upgrade Volumes from 2.4.92 ####"
-#                 ;;
-#             *)
-#                 echo "Unknown Version, upgrade not possible."
-#                 exit
-#                 ;;
-#             esac
-#             ############ DO ANY!!!
-#         fi
-#     done
-#     echo
-# }
+upgrade(){
+    for i in $FOLDER_with_VERSIONS
+    do
+        if [ ! -f "$i/${NAME}" ] 
+        then
+            # File not exist and now it will be created
+            command echo "${VERSION}" > "$i/${NAME}"
+        elif [ ! -f "$i/${NAME}" ] && [ -z "$(cat "$i/${NAME}")" ]
+        then
+            # File exists, but is empty
+            command echo "${VERSION}" > "$i/${NAME}"
+        elif [ "$VERSION" = "$(cat "$i/${NAME}")" ]
+        then
+            # File exists and the volume is the current version
+            command echo "Folder $i is on the newest version."
+        else
+            # upgrade
+            echo "Folder $i should be updated."
+            case "$i/$NAME" in
+            "1.4")
+                # Tasks todo in 2.4.92
+                echo "#### Upgrade Volumes from 2.4.92 ####"
+                ;;
+            *)
+                echo "Unknown Version, upgrade not possible."
+                exit
+                ;;
+            esac
+            ############ DO ANY!!!
+        fi
+    done
+    echo
+}
 
 #####################   MAIN    ###################
 # generate vars_common
-echo "$STARTMSG Create variables file..." && file_vars_common
+echo "Create variables file..." && file_vars_common
 # generate global_allow_IPs
-echo "$STARTMSG Create file for IP restrictions..." && file_GLOBAL_allow_IPs "$IP"
+echo "Create file for IP restrictions..." && file_GLOBAL_allow_IPs "$IP"
 # check if ssl cert is required to generate
-echo "$STARTMSG Check if cert is required..." && SSL_generate_cert
+echo "Check if cert is required..." && SSL_generate_cert
 # check if DH file is required to generate
-echo "$STARTMSG Check if DH is required..." && SSL_generate_DH
+echo "Check if DH is required..." && SSL_generate_DH
 # create maintenance file
-echo "$STARTMSG Create maintenance file..." && file_maintenance_html
+echo "Create maintenance file..." && file_maintenance_html
 # check volumes and upgrade if it is required
 #echo "$STARTMSG check if upgrade is required..." && upgrade
 
